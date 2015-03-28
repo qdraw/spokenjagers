02c43a0abb106c9f612752c63b8c974f19e38cd6
Dion <dionvanvelde@gmail.com>
2015-03-28 18:59:06 +0100

    add socket.io ++ start app

--- server.js
+++ server.js
@@ -1,80 +1,71 @@
-HOST = null; // localhost
-PORT = 8080;
-var PREVIOUS_NUM = 10; // the number of previous points to show
-var starttime = (new Date()).getTime();// start time
+// Servercode
 
-var fu = require("./fu"),
-    util = require("util"),
-    url = require("url"),
-    qs = require("querystring");
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var server;
 
-fu.listen(Number(process.env.PORT || PORT), HOST);
+server = http.createServer(function(req, res){
+    // your normal server code
+    var path = url.parse(req.url).pathname;
 
-fu.get("/", fu.staticHandler("index.html"));
-fu.get("/style.css", fu.staticHandler("style.css"));
-fu.get("/red.png", fu.staticHandler("red.png"));
-fu.get("/custom.js", fu.staticHandler("custom.js"));
+    fs.readFile(__dirname + path, function(err, data){
+        if (err){ 
+            return send404(res);
+        }
+        // res.writeHead(200, {'Content-Type': path == '.js' ? 'text/javascript' : 'text/html'});
 
-var channel = new function () {
-  var messages = [],callbacks = [];
+        
+        res.write(data, 'utf8');
+        res.end();
+    });
 
-  this.appendMessage = function (lat,long) {
-    var m = { lat: lat
-            , long: long
-            , timestamp: (new Date()).getTime()
-            };
-    util.puts('New Point:'+lat+' '+long);
+    // var path = url.parse(req.url).pathname;
+    // switch (path){
+    //     case '/':
+    //         res.writeHead(200, {'Content-Type': 'text/html'});
+    //         res.write('<h1>Hello! Try the <a href="/test.html">Test page</a></h1>');
+    //         res.end();
+    //         break;
+    //     case '/test.html':
+    //         fs.readFile(__dirname + path, function(err, data){
+    //             if (err){ 
+    //                 return send404(res);
+    //             }
+    //             res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
+    //             res.write(data, 'utf8');
+    //             res.end();
+    //         });
+    //     break;
+    //     default: send404(res);
+    // }
+}),
 
-    messages.push( m );
-
-    while (callbacks.length > 0) {
-      callbacks.shift().callback([m]);
-    }
-
-    while (messages.length > PREVIOUS_NUM)
-      messages.shift();
-  };
-
-  this.query = function (since, callback) {
-    var matching = [];
-    for (var i = 0; i < messages.length; i++) {
-      var message = messages[i];
-      if (message.timestamp > since)
-        matching.push(message)
-    }
-
-    if (matching.length != 0) {
-      callback(matching);
-    } else {
-      callbacks.push({ timestamp: new Date(), callback: callback });
-    }
-  };
-
-  // clear old callbacks for at most 30 seconds.
-  setInterval(function () {
-    var now = new Date();
-    while (callbacks.length > 0 && now - callbacks[0].timestamp > 30*1000) {
-      callbacks.shift().callback([]);
-    }
-  }, 3000);
+send404 = function(res){
+    res.writeHead(404);
+    res.write('404');
+    res.end();
 };
 
-fu.get("/pull", function (req, res) {
-  if (!qs.parse(url.parse(req.url).query).since) {
-    res.simpleJSON(400, { error: "Must supply since parameter" });
-    return;
-  }
+server.listen(8080);
 
-  var since = parseInt(qs.parse(url.parse(req.url).query).since, 10);
+// use socket.io
+var io = require('socket.io').listen(server);
 
-  channel.query(since, function (messages) {
-    res.simpleJSON(200, { messages: messages});
-  });
-});
+//turn off debug
+// io.set('log level', 1);
 
-fu.get("/push", function (req, res) {
-  var lat = qs.parse(url.parse(req.url).query).lat;
-  var long = qs.parse(url.parse(req.url).query).long;
-  channel.appendMessage(lat,long);
-  res.simpleJSON(200, { rss: '' });
+// define interactions with client
+io.sockets.on('connection', function(socket){
+
+    //send data to client
+    setInterval(function(){
+        socket.emit('date', {'date': new Date()});
+    }, 1000);
+
+    //recieve client data
+    socket.on('geo', function(data){
+        process.stdout.write(data.letter);
+    });
+
 });
