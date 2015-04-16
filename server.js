1a3ceac9336d47e74e4c810ea8d71aa8cd28fc92
Dion <dionvanvelde@gmail.com>
2015-04-16 15:24:04 +0200

    include db.js in server.js ++ update package.json

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ var fs = require('fs');
 var server;
 var public_html = "/public_html/"
 
-// Include:
-var database = require('./database.js');
-
-
 
 //star server using http
 server = http.createServer(function(req, res){
@@ -184,7 +180,7 @@ io.sockets.on('connection', function(socket){
 
             // Score first time user login
             // requires database.js
-            database.checkIfUserExist(userid);
+            checkIfUserExist(userid);
 
             isFirstRun = false;
         };
@@ -564,7 +560,7 @@ io.sockets.on('connection', function(socket){
                         // send score to db
 
                         // requires database.js
-                        database.updatePoints("score", userid);
+                        updatePoints("score", userid);
 
 
                         // send score to user:
@@ -597,7 +593,7 @@ io.sockets.on('connection', function(socket){
 
     var r1efreshIntervalId = setInterval(function () {
         if (global["checkIfUserExist_" + userid]) {
-            database.readScore(userid);
+            readScore(userid);
             clearInterval(r1efreshIntervalId);
         };
     },10)
@@ -881,3 +877,181 @@ console.log("> Local Server IP: " + addresses);
 
 
 console.log("> Script loaded");
+
+
+
+// db.js
+
+var dblite = require('dblite'),
+    db = dblite('db.sqlite');
+
+db.query('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, userid TEXT, health INTEGER, score INTEGER, money INTEGER, useragent TEXT, value TEXT)');
+
+// Only if you run d*b.js directly
+// checkIfUserExist ("dion");
+
+function checkIfUserExist (userid) {
+    global["rows_" + userid] = NaN;
+    global["checkIfUserExist_" + userid] = false;
+
+    db.query('SELECT * FROM users WHERE userid = ?',
+      [userid],
+      {
+        id: Number,
+        userid: String
+      },
+      function (rows) {
+        global["rows_" + userid] = rows;
+      }
+    );
+
+
+    var refreshIntervalId = setInterval(function () {
+        if (typeof global["rows_" + userid] == "object") {
+            rows = global["rows_" + userid];
+
+            var i = 0;
+            Object.keys(rows).forEach(function(key) {
+                console.log(rows[key]);
+                i++;
+            });
+
+            if (i == 1) {
+                global["checkIfUserExist_" + userid] = true;
+            };
+
+            if (i > 1 ) {
+                console.log("XXXXXX  ERROR more than one user with the same name");
+            };
+
+            if (i < 1) {
+                console.log("> User " + userid + " going to added to DB");
+                addNewUser (userid);
+            };
+
+        clearInterval(refreshIntervalId);
+        };
+    },10);
+
+}//e/CheckIfUserExist
+
+
+function addNewUser (userid) {
+    // NEVER USE THIS FUNCTION DIRECTLY!
+
+    // First Check if user exist
+    
+    global["latestid_" + userid] = NaN;
+
+    var latestid = NaN;
+
+    db.query('SELECT MAX(id) FROM users', function (err, rows) {
+        latestid = rows[0][0]
+        if (isNaN(latestid) ) {
+            console.log("error NaN");
+            latestid = 5000;
+        };
+        global["latestid_" + userid] = latestid;
+    });
+
+    var refreshIntervalId = setInterval(function () {
+        if (!isNaN(global["latestid_" + userid]) ) {
+
+            latestid = global["latestid_" + userid];
+            latestid++;
+
+            db.query('INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)',
+              [latestid, userid, 100, 0, 0, null, null]
+            );
+            global["checkIfUserExist_" + userid] = true;
+            clearInterval(refreshIntervalId);
+
+        }
+    },10)
+
+}
+
+// only for running d*b.js directly
+// readScore("dion");
+
+global["score"] = {};
+global["health"] = {};
+global["money"] = {};
+
+function readScore (userid) {
+    global["readScore_" + userid] = false;
+
+    var refreshIntervalId = setInterval(function () {
+        if (global["checkIfUserExist_" + userid]) {
+
+            db.query('SELECT * FROM users WHERE userid = ?', [userid], {
+                id: Number,
+                userid: String,
+                health: Number,
+                score: Number,
+                money: Number
+            }, function (err, rows) {
+                var record = rows[0];
+                
+                if (isNaN(record.score)) {
+                    global["score"][userid] = 0;
+                } else {
+                    global["score"][userid] = record.score;
+                }
+
+                if (isNaN(record.health)) {
+                    global["health"][userid] = 0;
+                } else {
+                    global["health"][userid] = record.health;
+                }
+
+                if (isNaN(record.money)) {
+                    global["money"][userid] = 0;
+                } else {
+                    global["money"][userid] = record.money;
+                }
+                global["readScore_" + userid] = true;
+
+                // global["score"][userid] = record.score;
+                // global["health"][userid] = record.health;
+                // global["money"][userid] = record.money;
+
+            });
+
+            clearInterval(refreshIntervalId);
+
+        };
+    },10)
+
+
+}//e//readScore
+
+// Read Score + on Login
+
+
+// Write Score Read Score +1
+
+// setTimeout( function () {
+//         updatePoints ("score","dion");
+// },40);
+
+
+function updatePoints (type,userid) {
+
+    var refreshIntervalId = setInterval(function () {
+        if (global["checkIfUserExist_" + userid]) {
+            
+            if (type === "score" || type === "health" || type === "money" || type === "useragent") {
+                db.query("UPDATE users SET "+ type +" = '" + global["score"][userid] + "' WHERE userid = '" + userid +"'");
+
+                // http://www.tutorialspoint.com/sql/sql-update-query.htm
+                // UPDATE CUSTOMERS SET ADDRESS = 'Pune' WHERE ID = 6;
+
+                clearInterval(refreshIntervalId);
+            };//e/score
+        };
+    },10)
+
+}//e/e/updatePoints
+
+
