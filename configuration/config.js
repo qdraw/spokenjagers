6b7808345a58257933ab6a852059f65e47fe75ca
Dion <dionvanvelde@gmail.com>
2015-04-17 14:40:19 +0200

    big changes in rep. ++ start new feature Facebook login ++ Express 4.x server ++ Waring mysql keys are online!! ++ move old items to old

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key" 		: 	"1377681645895429",
+	"facebook_api_secret"	:	"0244633f6bde3cd4043186b8499ed303",
+	"callback_url"			:	"http://localhost:8080/auth/facebook/callback",
+	"use_database"			:	"true",
+	"host"					:	"s84.webhostingserver.nl",
+	"username"				:	"deb40577_game",
+	"password"				:	"Gpw5KPrE",
+	"database"				:	"deb40577_game"
+}
